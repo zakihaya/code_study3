0b8039f108ff85c2a6d59bcc2a0623c87c3459b1
hayazaki <toru.hayazaki@gmail.com>
2015-09-27 20:24:54 +0900

    create www.ekidata.jp interface class

---
+++ app/models/station.rb
@@ -0,0 +1,16 @@
+class Station
+  require 'rexml/document'
+
+  STATIONS_URL = 'http://www.ekidata.jp/api/l/11302.xml'
+
+  def self.list
+    # 駅データAPIから山手線の駅一覧を取得
+    xml_data = Net::HTTP.get_response(URI.parse(STATIONS_URL)).body
+    Hash.from_xml(xml_data.to_s)['ekidata']['station']
+  end
+
+  # 駅名から駅情報を取得
+  def self.find_by_name(station_name)
+    list.select{ |s| s['station_name'] == station_name }.first
+  end
+end
