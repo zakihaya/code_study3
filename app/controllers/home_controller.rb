baf2c169d65c520ff334984f1e2545d4f139648d
hayazaki <toru.hayazaki@gmail.com>
2015-09-27 21:10:29 +0900

    show shop list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if params[:station]
+      selected_station = Station.find_by_name params['station']
+      @shops = HalalShop.list selected_station['lat'], selected_station['lon']
+    end
   end
 end
